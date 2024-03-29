47c363997be96641c57c7fe9eee719c5cb8e1f34
yazankills <138024063+yazankills@users.noreply.github.com>
2024-03-29 14:39:19 +0200

    heart.c

---
+++ heart.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <math.h>
+
+int size;
+
+void draw();
+
+char* s = "*";
+
+int main(void)
+{
+    printf("size (1-9): ");
+    if (scanf("%d", &size) != 1 || (size<1 || size>9))
+    {
+        return 1;
+    }
+
+    draw();
+
+    return 0;
+}
+
+void draw()
+{
+
+    //first row
+
+    printf("  ");
+
+    for(int i = 0; i<size-2; i++)
+    {
+
+        printf("%s",s);
+
+    }
+
+    for(int i = 0; i<size+2; i++)
+    {
+
+        printf(" ");
+
+    }
+
+    for(int i = 0; i<size-2; i++)
+    {
+
+        printf("%s",s);
+
+    }
+
+    printf("  \n");
+
+    //second row
+
+    printf(" ");
+
+    for(int i = 0; i<size; i++)
+    {
+
+        printf("%s",s);
+
+    }
+
+    for(int i = 0; i<size; i++)
+    {
+
+        printf(" ");
+
+    }
+
+    for(int i = 0; i<size; i++)
+    {
+
+        printf("%s",s);
+
+    }
+
+    printf(" \n");
+
+    //rest
+
+    for(int i = 0; i<size; i++)
+    {
+
+        for(int j = 0; j<((2*size) + 3 + (size-1)); j++)
+        {
+
+            printf("%s",s);
+
+        }
+
+        printf("\n");
+
+    }
+
+    if(size % 2 == 0)
+    {
+
+        for(int i = 0; i<(size*1.5); i++)
+        {
+
+            for(int j = 0; j<(size-(size-1-i)); j++)
+            {
+
+                printf(" ");
+
+            }
+
+            for(int j = 0; j<(((2*size) + 3 + (size-1)) - 2) - 2*i; j++)
+            {
+
+                printf("%s",s);
+
+            }
+
+           printf("\n");
+
+        }
+
+
+    }else{
+
+        for(int i = 0; i<(((3*size) + 1) / 2); i++)
+        {
+
+            for(int j = 0; j<(size-(size-1-i)); j++)
+            {
+
+                printf(" ");
+
+            }
+
+            for(int j = 0; j<(((2*size) + 3 + (size-1)) - 2) - 2*i; j++)
+            {
+
+                printf("%s",s);
+
+            }
+
+           printf("\n");
+
+        }
+    }
+
+}
